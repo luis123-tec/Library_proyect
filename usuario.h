581199b35d3d6ce1715f526acda4f8b892e09b37
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-01 23:40:25 -0600

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -1,12 +1,26 @@
+#ifndef USUARIO_H
+#define USUARIO_H
 #include "persona.h"
+#include <vector>
+#include "producto.h"
+#include "inventario.h"
 
 class Usuario : public Persona {
   private:
     int identificacion;
+    Inventario* inventario;
+
   public:
     Usuario();
     Usuario(int);
+    Usuario(Inventario* inv);
     int getIdentificacion();
     void setIdentificacion(int id);
     std::string getTipoPersona() override;
+    void visualizarInventario() override;
+    void modificarInventario() override;
+
+
 };
+
+#endif
