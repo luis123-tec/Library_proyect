9e58d9978e466359d85bc4f0a18cd4cc68225c23
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-01 23:32:45 -0600

    Update libro.h

--- libro.h
+++ libro.h
@@ -1,4 +1,8 @@
 
+#ifndef LIBRO_H
+#define LIBRO_H
+
+
 #include "producto.h"
 #include <string>
 
@@ -14,4 +18,9 @@ class Libro : public Producto {
     void setAnio(int year);
     void setAutor(std::string author);
     std::string getTipoProducto() override; //override que se utiliza para darle individualidad a libro y manga
+    void displayInfo(); //Hacer el metodo display, y luego hacer otro metodo para agregarle mas cosas
+    void displayInfo(int year, std::string author);
 };
+
+
+#endif
