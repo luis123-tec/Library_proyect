2275aef17b78f4854ca53b3e81fb38a909eaba50
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-01 23:31:12 -0600

    Update empleado.h

--- empleado.h
+++ empleado.h
@@ -1,13 +1,27 @@
+#ifndef EMPLEADO_H
+#define EMPLEADO_H
+
+
 #include "persona.h"
+#include <vector>
+#include "producto.h"
+#include "inventario.h"
 
 
 class Empleado : public Persona {
   private:
     int num_empleado;
+    Inventario* inventario;
+    
   public:
     Empleado();
     Empleado(int);
+    Empleado(Inventario* inv);
     int getNumEmpleado();
     void setNumEmpleado(int);
     std::string getTipoPersona() override;
+    void visualizarInventario() override;
+    void modificarInventario() override;
 };
+
+#endif
