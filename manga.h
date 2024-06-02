0c251cc850c3f0db810b16c5c63a5816460a2a3b
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-01 23:34:57 -0600

    Update manga.h

--- manga.h
+++ manga.h
@@ -2,11 +2,8 @@
 #define MANGA_H
 
 
-
 #include "producto.h"
 #include <string>
-
-
 
 class Manga : public Producto {
   private:
@@ -21,6 +18,8 @@ class Manga : public Producto {
     void setVolumenes(int vols);
     void setCalificacion(int cal);
     std::string getTipoProducto() override;
+    void displayInfo(); 
+    void displayInfo(int vols, int cal); // Sobrecarga
 };
 
 #endif
