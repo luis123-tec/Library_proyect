1dbe489f02f1f493e3f8257dbe4cdfc35eb61190
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-01 23:34:23 -0600

    Update manga.cpp

--- manga.cpp
+++ manga.cpp
@@ -24,3 +24,16 @@ void Manga::setCalificacion(int cal) {
 std::string Manga::getTipoProducto() {
   return "Manga";
 }
+void Manga::displayInfo() {
+  
+  Producto::displayInfo();
+  std::cout << "  Tipo: Manga" << std::endl; 
+}
+void Manga::displayInfo(int vols, int cal) {
+  
+  Producto::displayInfo();
+  std::cout << "  Tipo: Manga" << std::endl;
+  std::cout << "  Volumenes: " << vols << std::endl;
+  std::cout << "  Calificación: " << cal << std::endl;
+}
+
