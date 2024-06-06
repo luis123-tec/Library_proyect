9831fbd1fe5952f721302ec23778f6abccea0b7b
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-05 23:18:59 -0600

    Update usuario.cpp

--- usuario.cpp
+++ usuario.cpp
@@ -1,29 +1,24 @@
 #include "usuario.h"
 #include <iostream>
 
-
-
-Usuario::Usuario() {}
-
-Usuario::Usuario(int id) : identificacion(id) {}
-
-Usuario::Usuario(Inventario* inv) : inventario(inv) {}
+Usuario::Usuario(std::string nom, int age, int id, Inventario* inv)
+    : Persona(nom, age), identificacion(id), inventario(inv) {}
 
 int Usuario::getIdentificacion() {
-  return identificacion;
+    return identificacion;
 }
 
 void Usuario::setIdentificacion(int id) {
-  identificacion = id;
+    identificacion = id;
 }
+
 std::string Usuario::getTipoPersona() {
-  return "Comprador";
+    return "Comprador";
 }
+
 void Usuario::visualizarInventario() {
-  std::cout << "Cliente puede ver inventario." << std::endl;
-  inventario->mostrarInventario();
+    std::cout << "Cliente puede ver inventario." << std::endl;
+    inventario->mostrarInventario();
 }
-void Usuario::modificarInventario() {
-  std::cout << "Cliente no puede modificar el inventario." << std::endl;
-  inventario->modificarInventario();
-}
+
+void Usuario::modificarInventario() {}
