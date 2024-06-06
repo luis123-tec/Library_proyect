bdb7af4c5bd0725df42087a9eb40282e9f6014cd
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-05 23:16:02 -0600

    Update persona.cpp

--- persona.cpp
+++ persona.cpp
@@ -1,22 +1,7 @@
 #include "persona.h"
-#include <iostream>
-
-Persona::Persona() : nombre(""), edad(0) {}
 
 Persona::Persona(std::string nom, int age) : nombre(nom), edad(age) {}
 
-void Persona::setNombre(std::string nom) {
-  nombre = nom;
-}
-void Persona::setEdad(int age) {
-  edad = age;
-}
-std::string Persona::getNombre() {
-  return nombre;
-}
-int Persona::getEdad() {
-  return edad;
-}
-void Persona::displayData() {
-  std::cout << "";
+void Persona::mostrarDatos() const {
+    std::cout << "Nombre: " << nombre << "\nEdad: " << edad << std::endl;
 }
