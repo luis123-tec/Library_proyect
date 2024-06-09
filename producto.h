6bcd122d37c272bb332071ca4434e6ad95797c5b
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2024-06-09 16:59:45 -0600

    Update producto.h

--- producto.h
+++ producto.h
@@ -14,13 +14,14 @@ class Producto {
   public:
     Producto();
     Producto(std::string tit, double prc, int stk);
+    friend std::ostream& operator<<(std::ostream& out, const Producto&  product); //sobrecarga de operador
     std::string getTitle();
     double getPrice();
     int getStock();
     void setTitle(std::string tit);
     void setPrice(double prc);
     void setStock(int stock);
-    virtual void displayInfo() const; //Polimorfismo, sin clases virtuales puras
+    virtual void displayInfo() const; //sobreescritura
     virtual std::string getTipoProducto() = 0; // Método virtual que posteriormente se va a cambiar con override CLASES ABSTRACTAS
 };
 
